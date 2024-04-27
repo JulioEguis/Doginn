d16188add49db446d78acf9aa33444067e6a1756
JulioEguis <144676330+JulioEguis@users.noreply.github.com>
2024-04-27 18:23:02 +0100

    Delete Doginn-master directory

--- Doginn-master/miweb.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Redirige al usuario a la página index_main.php
-header('Location: index_main.php');
-
-// Incluye el archivo de conexión a la base de datos
-include 'includes/conexion.php';
-?>
